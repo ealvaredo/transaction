36fdc9163850063bc5cd14e29d3da32c20be80f4
Emanuel Alvaredo <ealvaredo@gmail.com>
2019-10-22 17:23:33 -0300

    formateo de estado

--- src/app/pipe/StatusPipe.ts
+++ src/app/pipe/StatusPipe.ts
@@ -1,14 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'status' })
-export class StatusPipe implements PipeTransform  {
+export class StatusPipe implements PipeTransform {
 
     transform(status: string) {
         if (status == 'REQ_FROM_ENT_WITH_USER_AND_ACCEPTED_BY_USER_PAY_AFTER_CONFIRMED_BY_ENT') {
             return "PENDIENTE";
+        } else if (status == 'REQ_FROM_ENT_WITH_USER_AND_ACCEPTED_BY_USER_PAY_AFTER_CONFIRMED_BY_ENT_PAID_BY_USER' || status == 'REQ_FROM_SEAC_APPROVED_DIRECTLY') {
+            return "REALIZADA";
         } else {
             return status;
         }
     }
-    
+
 }
